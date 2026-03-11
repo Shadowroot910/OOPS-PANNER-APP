443d85541df53c8ec1d8cdb3396a79c8762b46fd
Shadowroot910 <verse20245@gmail.com>
2026-03-11 08:36:01 +0530

    uc2

---
+++ Stringjoin.java
@@ -0,0 +1,57 @@
+public class Stringjoin {
+    public static void main(String[] args) {
+
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "
+        ));
+
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **     **",
+                " **      "
+        ));
+
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **     **",
+                " **      "
+        ));
+
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " ******  ",
+                "  *****   "
+        ));
+
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **       ",
+                "       ** "
+        ));
+
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **       ",
+                "       ** "
+        ));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " **       ",
+                "  ****** "
+        ));
+    }
+
+    
+}
+
