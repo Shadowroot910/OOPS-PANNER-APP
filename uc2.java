aaeebe4c182544cb18797bbb3b306ee073ddbca4
Shadowroot910 <verse20245@gmail.com>
2026-03-11 08:42:19 +0530

    uc2

---
+++ uc2.java
@@ -0,0 +1,14 @@
+public class uc2 {
+       public static void main(String[] args) {
+        /* Use case 2 */
+        System.out.println(" **    **    **    *** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   **    ***  ");
+        System.out.println("*     *  *     *   *              * ");
+        System.out.println("*     *  *     *   *        *     * ");
+        System.out.println(" **    **    *         ***  ");
+
+    }
+    
+}
